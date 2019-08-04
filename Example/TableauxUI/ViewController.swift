98087fe73e93820c7ec979ff85c42d9d868c7a4d
Ernest DeFoy III <erniedefoy3@gmail.com>
2019-08-04 08:53:10 -0400

    add dummy class

--- Example/TableauxUI/ViewController.swift
+++ Example/TableauxUI/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import TableauxUI
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+	
+	var isBlinking = false
+	let blinkingLabel = BlinkingLabel(frame: CGRect(x: 10, y: 20, width: 200, height: 30))
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Setup the BlinkingLabel
+		blinkingLabel.text = "I blink!"
+		blinkingLabel.font = UIFont.systemFont(ofSize: 20)
+		view.addSubview(blinkingLabel)
+		blinkingLabel.startBlinking()
+		isBlinking = true
+		
+		// Create a UIButton to toggle the blinking
+		let toggleButton = UIButton(frame: CGRect(x: 10, y: 60, width: 125, height: 30))
+		toggleButton.setTitle("Toggle Blinking", for: .normal)
+		toggleButton.setTitleColor(.red, for: .normal)
+		view.addSubview(toggleButton)
+	}
+	
+	func toggleBlinking() {
+		if (isBlinking) {
+			blinkingLabel.stopBlinking()
+		} else {
+			blinkingLabel.startBlinking()
+		}
+		isBlinking = !isBlinking
+	}
+	
 }
 
